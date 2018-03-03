ab5beb8c0a7a5719bfc706f6608d1e2695ed4aca
unknown <zubinchoudhary@gmail.com>
2018-03-03 15:48:33 +0530

    Kinect-ify Attempt

---
+++ PaddlePopv1/Assets/Scripts/KinectManager1.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using Windows.Kinect;
+
+using System.Linq;
+
+public class KinectManager1 : MonoBehaviour {
+
+	private KinectSensor _sensor;
+	private BodyFrameReader _bodyFrameReader;
+	private Body[] _bodies = null;
+
+	public GameObject kinectAvailableText;
+	public Text handXText;
+
+	public bool isAvailable;
+	public float PaddlePosition;
+	public bool isFire;
+
+	public static KinectManager1 instance = null;
+
+	public Body[] GetBodies()
+	{
+		return _bodies;
+	}
+		
+	void Awake () {
+		if(instance == null)
+			instance = this;
+		else if(instance != this)
+			Destroy(gameObject);
+	}
+
+	void Start(){
+		_sensor = KinectSensor.GetDefault ();
+
+		if (_sensor != null) {
+			isAvailable = _sensor.IsAvailable;
+			kinectAvailableText.SetActive (isAvailable);
+			_bodyFrameReader = _sensor.BodyFrameSource.OpenReader ();
+			if (!_sensor.IsOpen)
+				_sensor.Open ();
+			_bodies = new Body[_sensor.BodyFrameSource.BodyCount];
+		}
+	}
+		
+	void Update () {
+		isAvailable = _sensor.IsAvailable;
+
+		if (_bodyFrameReader != null) {
+			var frame = _bodyFrameReader.AcquireLatestFrame ();
+
+			if(frame != null){
+				frame.GetAndRefreshBodyData(_bodies);
+				foreach(var body in _bodies.Where(b=>b.IsTracked))
+				{
+					isAvailable = true;
+					if(body.HandRightConfidence == TrackingConfidence.High && body.HandRightState == HandState.Lasso)
+					{
+						isFire = true;
+					}	
+					else
+					{
+						PaddlePosition = RescalingToRangesB(-1,1,-8,8, body.Lean.X);
+						handXText.text = PaddlePosition.ToString();
+					}
+				}
+				frame.Dispose();
+				frame = null;
+			}
+		}
+	}
+
+	static float RescalingToRangesB(float scaleAStart, float scaleAEnd, float scaleBStart, float scaleBEnd, float valueA){
+		return (((valueA - scaleAStart) * (scaleBEnd - scaleBStart)) / (scaleAEnd - scaleAStart)) + scaleBStart;
+	}
+
+	void OnAppliationQuit()
+	{
+		if (_bodyFrameReader != null) {
+			_bodyFrameReader.IsPaused = true;
+			_bodyFrameReader.Dispose ();
+			_bodyFrameReader = null;
+		}
+
+		if (_sensor != null) {
+			if (_sensor.IsOpen)
+				_sensor.Close ();
+		}
+		_sensor = null;
+	}
+}
---
+++ PaddlePopv1/Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+
+	public float PaddleSpeed = 1f;
+	private Vector3 playerPos = new Vector3 (0f, -9.5f, 0f);
+
+	// Update is called once per frame
+	void Update () {
+		float xPos = transform.position.x;
+
+		if (KinectManager1.instance.isAvailable)
+			xPos = KinectManager1.instance.PaddlePosition;
+		else
+			xPos = transform.position.x + (Input.GetAxis ("Horizontal") * PaddleSpeed);
+		playerPos = new Vector3 (Mathf.Clamp (xPos, -8f, 8f), -9.5f, 0f);
+		transform.position = playerPos;
+	}
+}
